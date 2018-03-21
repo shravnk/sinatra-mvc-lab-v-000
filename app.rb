40b1c6941885b7c47ba5e3c0771422adf72bf0c8
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-21 03:27:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    input = params['user_phrase']
+    word = PigLatinizer.new
+    @new_latin = word.piglatinize(input)
+    erb :piglatinize
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,54 @@
+class PigLatinizer
+  attr_accessor :to_latin
+  def initialize
+
+  end
+
+  def piglatinize(sent)
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        words = sent.split(' ')
+        result = []
+
+    words.each_with_index do |word, i|
+        translation = ''
+        qu = false
+        if vowels.include? word[0].downcase
+            translation = word + 'way'
+            result.push(translation)
+        else
+            word = word.split('')
+            count = 0
+            word.each_with_index do |char, index|
+                if vowels.include? char
+                    # handle words that start with 'qu'
+                    if char == 'u' and translation[-1] == 'q'
+                        qu = true
+                        translation = words[i][count + 1..words[i].length] + translation + 'uay'
+                        result.push(translation)
+                        next
+                    end
+                    break
+                else
+                    # handle words with 'qu' in middle
+                    if char == 'q' and word[i+1] == 'u'
+                        qu = true
+                        translation = words[i][count + 2..words[i].length] + 'quay'
+                        result.push(translation)
+                        next
+                    else
+                        translation += char
+                    end
+                    count += 1
+                end
+            end
+            # translation of consonant words without qu
+            if not qu
+                translation = words[i][count..words[i].length] + translation + 'ay'
+                result.push(translation)
+            end
+        end
+
+    end
+    result.join(' ')
+  end
+end
