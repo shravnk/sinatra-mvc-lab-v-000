1448b20019e7a0cdef732d2003a7f81266505864
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-21 03:04:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/' do
     erb :user_input
+  end
 
+  post '/piglatinize' do
+    @input = params['user_phrase']
+
+    erb :piglatinize
   end
 end
